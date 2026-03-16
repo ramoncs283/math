3cdac98553b0c6c0b8d31ab6a93aa3e923608190
ramoncs283 <ramon.zielke@outlook.com>
2026-03-16 11:13:47 +0100

    faction test geschrieben

--- fraction_test.ts
+++ fraction_test.ts
@@ -34,3 +34,39 @@ Deno.test("1/3 + 2/6 = 2/3 is roughly 0.67", () => {
   // Assert
   assertAlmostEquals(left.toFloat(0.01), 0.67);
 });
+
+Deno.test("3/3 - 1/3 = 2/3 is roughly 0.67", () => {
+  // Arrange
+  const left = new Fraction(3, 3);
+  const right = new Fraction(1, 3);
+
+  // Act
+  left.subtract(right);
+
+  // Assert
+  assertAlmostEquals(left.toFloat(0.01), 0.67);
+});
+
+Deno.test("2/3 * 2/3 = 4/9 is roughly 0.444", () => {
+  // Arrange
+  const left = new Fraction(2, 3);
+  const right = new Fraction(2, 3);
+
+  // Act
+  left.multiply(right);
+
+  // Assert
+  assertAlmostEquals(left.toFloat(0.001), 0.444);
+});
+
+Deno.test("10/3 / 2/3 = 5/3 is roughly 2.0", () => {
+  // Arrange
+  const left = new Fraction(10, 3);
+  const right = new Fraction(5, 3);
+
+  // Act
+  left.divide(right);
+
+  // Assert
+  assertAlmostEquals(left.toFloat(0.1), 2.0);
+});
